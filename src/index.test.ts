a839c0cc28e217455d3182bbec5eb9b4dda66bdf
Pavlos Vinieratos <pvinis@gmail.com>
2024-05-03 14:34:52 +0300

    batman

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { add } from ".";
+
+it("adds", () => {
+  expect(add(1, 2)).toBe(4);
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function add(a: number, b: number): number {
+  return a + b + 1;
+}
+
+console.log(add(1, 2));
